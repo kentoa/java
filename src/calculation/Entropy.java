e44485fbb83af60df4b3759240deadb6c4e2a610
kentoa <k_akiyama@kde.cs.tsukuba.ac.jp>
2015-12-10 12:12:43 +0900

    second commit

--- src/calculation/Entropy.java
+++ src/calculation/Entropy.java
@@ -4,20 +4,18 @@ import java.util.Map;
 public class Entropy {
 	void calc_entropy(String str){
 		Map<String, Integer> chMap = new HashMap<>();
-		int count;
-		double entropy;
+		int count, lanlength = str.length();
+		double probability, entropy = 0;
 		
 		String ori = str;
 	
-		for(int i = 0; i < str.length(); i++){ //count occurrence for each characters
+		for(int i = 0; i < lanlength; i++){ //count occurrence for each characters
 			String ch = ori.substring(i, i + 1);
 			if(chMap.containsKey(ch)){
 				count = chMap.get(ch);
 				chMap.put(ch, ++count);
 			}
-			else{
-				chMap.put(ch, 1);
-			}
+			else chMap.put(ch, 1);
 		}
 		
 		int num = chMap.size();
@@ -27,15 +25,18 @@ public class Entropy {
 		
 		for(Map.Entry<String, Integer> e: chMap.entrySet()){
 			count++;
-			System.out.println(e.getKey() + ", " + e.getValue()); //show frequency for each characters
-			px[count] = e.getValue() / (double)num;
+			System.out.println("<key, freq> = <" + e.getKey() + ", " + e.getValue() + ">"); //show frequency for each characters
+			px[count] = e.getValue() / (double)lanlength;
 			ch[count] = e.getKey();
 		}
 		
 		for(int i = 0; i < num; i++){
-			entropy = -px[i] * Math.log(px[i]); //calculate entropy
-			System.out.println("<key, entoropy> = <" +ch[i] +  ", " + entropy + ">");
+			probability = -px[i] * Math.log(px[i]); //calculate entropy
+			System.out.println("<key, probability> = <" +ch[i] +  ", " + probability + ">");
+			entropy += probability;
 		}
+		System.out.println("entropy = " + entropy);
+		System.out.println("average code length = " + lanlength * entropy);
 	}
 	
 	public static void main(String[] args) {
